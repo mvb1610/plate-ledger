c13791a040230df0d09abf9f61595cb452f8ec04
mvb1610 <132676514+mvb1610@users.noreply.github.com>
2026-09-19 22:19:21 -0400

    Update cache version to v1.1.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'plate-ledger-v1.1.0';
+const CACHE = 'plate-ledger-v1.1.1';
 const SHELL = ['./', './index.html', './app.js', './manifest.json', './data/cnf.json', './data/usda.json', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 const FB = ['https://www.gstatic.com/firebasejs/12.3.0/firebase-app-compat.js', 'https://www.gstatic.com/firebasejs/12.3.0/firebase-auth-compat.js', 'https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore-compat.js'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL).then(() => Promise.allSettled(FB.map(u => fetch(u, { mode: 'cors' }).then(r => c.put(u, r)))))).then(() => self.skipWaiting())); });
